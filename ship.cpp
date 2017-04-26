b7561c1d9d7b10deb2e93d761aa6974ce0dc91c3
Igor` Kotel`nikov <webarts@yandex.ru>
2017-04-26 19:47:11 +0800

    Update ship.cpp

--- ship.cpp
+++ ship.cpp
@@ -1,12 +1,15 @@
 #include <iostream>
 #include "ship.h"
 #include <conio.h>
-void ship::kbht() {
+
+void ship::kbht() 
+{
 	int a = 0;
 	if (_kbhit())
 	{
 		a = _getch();
-		if (num == 2) {
+		if (num == 2) 
+		{
 			if ((a == 119) || (a == 230)) Vy = -1;
 			if ((a == 115) || (a == 235)) Vy = 1;
 		}
