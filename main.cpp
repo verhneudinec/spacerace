988f615ef4e64563710aa768f0b7a03f11528cb6
Igor` Kotel`nikov <webarts@yandex.ru>
2017-04-26 21:00:41 +0800

    Шрифт + размер шрифта
    Взято с http://www.cyberforum.ru/cpp-beginners/thread1674646.html#7

--- main.cpp
+++ main.cpp
@@ -5,11 +5,18 @@ int main()
 {
 	SetConsoleCP(1251);
 	SetConsoleOutputCP(1251); 
-	system("mode con cols=60 lines=30");
 	HANDLE hStdOut = GetStdHandle(STD_OUTPUT_HANDLE);
 	CONSOLE_CURSOR_INFO conCurInf;
 	GetConsoleCursorInfo(hStdOut, &conCurInf);
 	conCurInf.bVisible = false;
 	SetConsoleCursorInfo(hStdOut, &conCurInf);
+	CONSOLE_FONT_INFOEX fontInf;
+	fontInf.cbSize = sizeof(fontInf);
+	GetCurrentConsoleFontEx(hStdOut, TRUE, &fontInf);
+	wcscpy(fontInf.FaceName, L"Consolas");
+	fontInf.dwFontSize.Y = 20;
+	SetCurrentConsoleFontEx(hStdOut, TRUE, &fontInf);
+	system("mode con cols=60 lines=30");
+	
 	home();
 }
