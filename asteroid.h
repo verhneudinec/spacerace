d495066524af688a1d0019765660314f641cd560
Igor` Kotel`nikov <webarts@yandex.ru>
2017-04-26 19:48:00 +0800

    Update asteroid.h
    Учтены замечания Nougatman`a по читабельности кода :)

--- asteroid.h
+++ asteroid.h
@@ -1,26 +1,33 @@
 #include "header.h"
-class asteroid {
+class asteroid 
+{
 	string s;
 	int positionX;
 	int positionY; 
 	float Vx;
-
-    public:
-	asteroid() {
+	
+	public:
+	
+	asteroid() 
+	{
 		s = "*";
 		positionX = 0 + rand() % 60;
 		positionY = 3 + rand() % 20;
 		Vx = -1;
 	}
-	void display() {
+	
+	void display() 
+	{
 		moving();
 		setcurpos(positionX, positionY);
 		cout << s;
 		escape();
 		cout << " ";
 	}
-	void escape() {
-		if ((positionX < 1))
+	
+	void escape() 
+	{
+		if (positionX < 1)
 		{   
 			setcurpos(positionX, positionY);
 			cout << " ";
@@ -28,8 +35,12 @@ class asteroid {
 			positionY = 2 + rand() % 20;
 		}
 	}
-	void moving() {
+	
+	void moving() 
+	{
 		positionX += int(Vx);
 	}
+	
 	friend void checkblow(class asteroid *, class ship &);
+	
 };
